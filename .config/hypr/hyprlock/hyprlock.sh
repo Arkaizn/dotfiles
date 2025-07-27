f3fc04178b3669ed3930e01ef86b2978c63dc234
arkaizn <silas.gubler@hispeed.ch>
2025-07-27 15:23:05 +0200

    add general monitor

--- .config/hypr/hyprlock/hyprlock.sh
+++ .config/hypr/hyprlock/hyprlock.sh
@@ -3,6 +3,10 @@
 # Source the color environment variables so Hyprlock can use them
 source ~/.cache/wal/hyprlock_colors
 
+# set monitor
+export monitor="DP-2"
+
+
 # Run Hyprlock with the environment variables set
 hyprlock &
 
