dca179859940b074277e61abd17800e3dab0b922
Silas Gubler <silas.gubler@federtechnik.ch>
2025-10-01 14:57:59 +0200

    add notification for tailscale

--- .config/custom/scripts/tailscale-toggle.sh
+++ .config/custom/scripts/tailscale-toggle.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 if tailscale status 2>&1 | grep -q "Tailscale is stopped"; then
-    echo "Tailscale is down → bringing it up..."
     tailscale up
+    notify-send "Tailscale Up" "🔌 Tailscale is  now up."
 else
-    echo "Tailscale is up → bringing it down..."
     tailscale down
+    notify-send "Tailscale Down" "❌ Tailscale is now down."
 fi
+
