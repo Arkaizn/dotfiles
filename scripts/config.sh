e764ed0e289904fcc20746b442a9a71f36b248c0
Silas Gubler <silas.gubler@federtechnik.ch>
2025-10-17 16:08:11 +0200

    small changes

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
 # copy hypr config
-cp -fr ./.config/hypr ~/.config
-
-# copy alacritty config
-cp -fr ./.config/alacritty ~/.config
-
-# copy waybar config
-cp -fr ./.config/waybar ~/.config
-
-# copy fastfetch config
-cp -fr ./.config/fastfetch ~/.config
-
-# copy swaync config
-cp -fr ./.config/swaync ~/.config
-
-# copy wlogout config
-cp -fr ./.config/wlogout ~/.config
-
-# copy Wofi config
-cp -fr ./.config/wofi ~/.config
-
-# copy kitty config
-cp -fr ./.config/kitty ~/.config
-
-# copy Pywal config
-cp -fr ./.config/wal ~/.config
+cp -fr ./.config/* ~/.config
