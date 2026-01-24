4a2885a7b9b79c7abccacacd8d3309db70386775
arkaizn <silas.gubler@hispeed.ch>
2026-01-24 16:28:31 +0100

    shorten script, add hyprlock template

--- .config/hypr/wallpapers/set_wallpaper.sh
+++ .config/hypr/wallpapers/set_wallpaper.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-rm -fr /tmp/wallpaper_script.log
-LOG_FILE="/tmp/wallpaper_script.log"
-exec &> >(tee -a "$LOG_FILE")
-echo "=== Script started at $(date) ==="
-
 WALLPAPER="${HOME}/.config/hypr/wallpapers/pywallpaper.png"
 HYPRLOCK_COLORS_CACHE="${HOME}/.cache/wal/hyprlock_colors"
 
@@ -19,20 +14,11 @@ rm -fr ~/.cache/wal/schemes
 # Run pywal
 wal -i "$WALLPAPER" 2>/dev/null || true
 
-# Reload hyprland
-#echo reload
-#hyprctl reload
-
-
 # pywalfox / Firefox
 command -v pywalfox >/dev/null 2>&1 && pywalfox update 2>/dev/null || true
 
-# Hyprlock
-[[ -f "$HOME/.cache/wal/colors" ]] && awk '{hex=substr($0,2);r=strtonum("0x"substr(hex,1,2));g=strtonum("0x"substr(hex,3,2));b=strtonum("0x"substr(hex,5,2));printf"export color%d=\"rgb(%d,%d,%d)\"\n",NR-1,r,g,b}' "$HOME/.cache/wal/colors" > "$HYPRLOCK_COLORS_CACHE"
-
 # Quickshell
-cp ~/.cache/wal/Colors.qml ~/.config/quickshell
-chmod +x ~/.config/quickshell/Colors.qml
+[[ -e ~/.config/quickshell/Colors.qml ]] || ln -s ~/.cache/wal/Colors.qml ~/.config/quickshell/Colors.qml
 
 # Reload swaync
 command -v swaync-client >/dev/null 2>&1 && swaync-client --reload-css || true
@@ -41,6 +27,3 @@ command -v swaync-client >/dev/null 2>&1 && swaync-client --reload-css || true
 export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS:-}"
 source "$HOME/.cache/wal/colors.sh"
 openrgb --color "${color11#\#}" 2>/dev/null || true
-
-
-echo "=== Script ended at $(date) ==="
