d51ab085c466d8ca214948dad81ee17fcd61d58e
arkaizn <silas.gubler@hispeed.ch>
2026-05-12 00:10:38 +0200

    add different monitor configs

--- .config/hypr/hyprland/monitors.lua
+++ .config/hypr/hyprland/monitors.lua
@@ -1,4 +1,28 @@
 -- https://wiki.hypr.land/Configuring/Basics/Monitors/
 
-hl.monitor({output = "DP-1", mode = "1920x1080@165", position = "2560x0", scale = "1",})
-hl.monitor({output = "DP-2", mode = "2560x1440@360", position = "0x0", scale = "1",})
+-- hl.monitor({output = "DP-1", mode = "1920x1080@165", position = "2560x0", scale = "1",})
+-- hl.monitor({output = "DP-2", mode = "2560x1440@360", position = "0x0", scale = "1",})
+
+-- See https://wiki.hyprland.org/Configuring/Monitors/
+
+--hl.monitor({output = "DP-1", mode = "1920x1080@165", position = "2560x0", scale = "1",})
+--hl.monitor({output = "DP-2", mode = "2560x1440@360", position = "0x0", scale = "1",})
+--hl.monitor({output = "HDMI-A-1", mode = "1920x1080@75", position = "0x1440", scale = "1",})
+--hl.monitor({output = "HEADLESS-2", mode = "1920x1080@60", position = "300x1080", scale = "1.5",})
+--hl.monitor({output = "eDP-1", mode = "1920x1200@60", position = "0x0", scale = "1",})
+
+-- VMware Virtual Machine
+--hl.monitor({output = "Virtual-1", mode = "1718x920@60", position = "0x0", scale = "1",})
+
+-- Federi
+--hl.monitor({output = "DP-6", mode = "2560x1440@75", position = "1920x0", scale = "1",})
+--hl.monitor({output = "DP-5", mode = "2560x1440@75", position = "4480x0", scale = "1",})
+
+-- Using monitor descriptions
+--hl.monitor({output = "desc:AOC Q27P2W ASEQ59A000131", mode = "2560x1440@75", position = "1920x0", scale = "1",})
+--hl.monitor({output = "desc:AOC Q27P2W ASEQ59A000698", mode = "2560x1440@75", position = "4480x0", scale = "1",})
+
+-- Typo-fixed version of the malformed entry
+--hl.monitor({output = "DP-6", mode = "2560x1440@75", position = "1920x0", scale = "1",})
+
+--hl.monitor({output = "eDP-1", mode = "1920x1200@60", position = "7040x0", scale = "1",})
