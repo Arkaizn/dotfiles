0559603f1a75d4060df65cfdb4184ce70b5ad51f
arkaizn <silas.gubler@hispeed.ch>
2026-06-28 10:12:48 +0200

    remove hyprctl

--- scripts/config/update.sh
+++ scripts/config/update.sh
@@ -74,6 +74,6 @@ done
 # ── Reload ────────────────────────────────────────────────────────────────────
 echo "Reloading…"
 hyprctl reload || true
-pkill qs      && hyprctl dispatch "hl.dsp.exec_cmd('QS_NO_RELOAD_POPUP=1 qs')" && niri msg action spawn -- sh -c 'QS_NO_RELOAD_POPUP=1 qs' || true
+pkill qs      && niri msg action spawn -- sh -c 'QS_NO_RELOAD_POPUP=1 qs' || true
 
 echo "Done ✅"
