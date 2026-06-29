78d6a7f358d5777a35997055a62bd5610a4175e0
Arkaizn <silas.gubler@hispeed.ch>
2026-06-29 15:22:11 +0200

    add gpg key login

--- .config/custom/scripts/rdp-work.sh
+++ .config/custom/scripts/rdp-work.sh
@@ -1,3 +1,5 @@
-pw=$(zenity --password --title="RDP Password") && \
-[ -n "$pw" ] && \
-xfreerdp3 /ipv4 /v:pc02 /u:user /p:"$pw" /sec:tls:on /cert:ignore +dynamic-resolution +clipboard /drive:homedir,/home/name
+#!/bin/bash
+pw=$(pass show work/rdp-sig)
+xfreerdp3 /ipv4 /v:vmk009.federtechnik.ch /u:sig /p:"$pw" /sec:tls:on /cert:ignore /kbd:layout:0x00000807 +dynamic-resolution +clipboard /drive:homedir,/home/sig
+
+
